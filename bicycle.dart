288453b83cfdddfda6df3cbad413493682a5cbed
yanuarrizki1 <101191233+yanuarrizki1@users.noreply.github.com>
2022-03-08 20:20:36 +0700

    code.dart1

---
+++ bicycle.dart
@@ -0,0 +1,28 @@
+// Yanuar Rizki 144
+
+class Bicycle {
+  int cadence;
+  int _speed = 0;
+  int get speed => _speed;
+  int gear;
+
+  Bicycle(this.cadence, this.gear);
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
+  @override
+  String toString() => 'Bicycle: $_speed mph';
+}
+
+void main() {
+  var car  = Bicycle(5,2);
+  car.speedUp(200);
+  
+  print(car);
+}
